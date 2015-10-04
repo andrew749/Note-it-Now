551f0823b78467123d49682f537d268fc94983c1
Andrew Codispoti <andrewcod749@gmail.com>
2015-10-04 15:20:43 -0400

    Fixed path for google app engine.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(1, os.path.join(os.path.abspath("bin/lib/python2.7/site-packages")))
 from flask import Flask,jsonify, Response, session, request, redirect, render_template
 import flask
 import requests
