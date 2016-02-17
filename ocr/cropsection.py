e71194f00859038fbdc845f318808bd9ed9d255b
Andrew Codispoti <andrewcod749@gmail.com>
2016-02-16 22:00:17 -0800

    Added some padding to image crop.

--- ocr/cropsection.py
+++ ocr/cropsection.py
@@ -3,8 +3,9 @@ beginning and end are keypoints and imagePath is the name/path of the image
 """
 def getSection(image, beginning, end):
     left = int(beginning[0])
-    top = int(beginning[1])
+    top = 0 if beginning[1] < 30 else int(beginning[1]) - 30
     right = int(end[0])
-    bottom = int(end[1])
+    height = image.shape[0]
+    bottom = height if height - end[1] < 30 else int(end[1]) + 30
     cropped_example = image[top:bottom, left:right]
     return cropped_example
